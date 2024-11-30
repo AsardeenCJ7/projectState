d392f3b4f9b58bba48de4302594c648e3e91fc50
M.A. Asardeen <asardeenbse@gmail.com>
2024-11-30 21:33:33 +0530

    Multiple Form Input Handle

--- projectState/src/components/MyForm.jsx
+++ projectState/src/components/MyForm.jsx
@@ -3,8 +3,15 @@ import React, { useState } from "react";
 function MyForm() {
   const [name, setName] = useState("");
   // document.getElementById('fname').value; avoid this js code in react another method here we need to use
+
+  function handleSubmit(e) {
+    // onSubmit Also event Listener so that return e
+    e.preventDefault();
+    console.log("Form Submitted");
+    console.log("Current State", name);
+  }
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <h1>Online Registration Form</h1>
       <label htmlFor="fname">First Name : </label>
       <input
@@ -15,13 +22,7 @@ function MyForm() {
           setName(e.target.value);
         }}
       />
-      <button
-        onClick={() => {
-          setName();
-        }}
-      >
-        Submit
-      </button>
+      <input type="submit" value="Submit" />
 
       <h2>{name}</h2>
     </form>
